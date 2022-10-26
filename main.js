55787aff9569a146a13158e61c4af2c7527c44d0
Nikita Koliadin <qThegamEp@gmail.com>
2022-10-26 12:37:35 +0300

    Lesson 4

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg'
+            image: './assets/images/socks_green.jpg',
+            inventory: 100,
+            onSale: true
         }
     }
 })
