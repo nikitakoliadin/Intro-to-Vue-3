e043d1b0a609c3911f1f399c51b1cdf868133c08
Nikita Koliadin <qThegamEp@gmail.com>
2022-10-26 14:25:58 +0300

    Lesson 9 start

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
+            cart: 0,
             product: 'Socks',
             brand: 'Vue Mastery',
             selectedVariant: 0,
@@ -9,8 +9,7 @@ const app = Vue.createApp({
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-            ],
-            onSale: false
+            ]
         }
     },
     methods: {
@@ -31,11 +30,5 @@ const app = Vue.createApp({
         inStock() {
             return this.variants[this.selectedVariant].quantity
         },
-        sale() {
-            if (this.onSale) {
-                return this.brand + ' ' + this.product
-            }
-            return '';
-        }
     }
 })
